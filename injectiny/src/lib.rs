4c5e68f0b34a6d62190718c4ae1539ceabe41ddd
David Lenaerts <david.lenaerts@gmail.com>
2024-06-07 19:45:08 +0200

    Missing files

---
+++ injectiny/src/lib.rs
@@ -0,0 +1,108 @@
+//! # A tiny dependency injection library for Rust.
+//!
+//! This library provides a simple way to inject dependencies into structs.
+//! Injectiny is *not* a framework, but can be used inside one. Please refer to the README.md
+//! for an example of how to use it.
+//!
+//! # Installation
+//!
+//! To use Injectiny, make sure to include both the `injectiny` and `injectiny_proc_macro` crates.
+//!
+//! ```ignore
+//! cargo add injectiny
+//! cargo add injectiny_proc_macro
+//! ```
+//!
+//! # Example
+//!
+//! ```
+//!
+//! use std::cell::RefCell;
+//! use std::rc::Rc;
+//! use injectiny::{Injected, Injectable};
+//! use injectiny_proc_macro::injectable;
+//!
+//! // Model is an enum defining all the fields that can be injected
+//! #[derive(Clone)]
+//! enum Model {
+//!    Name(Rc<RefCell<String>>),   // non-clonable objects should be wrapped in Rc<RefCell<T>>
+//!    Age(u32)
+//! }
+//!
+//! // The #[injectable] attribute macro generates an implementation of the Injectable trait for
+//! // the given Model enum. In real situations, this could be one of several controllers, views, ...
+//! #[injectable(Model)]
+//! #[derive(Default)]
+//! struct Injectee
+//! {
+//!     // The #[inject] attribute macro marks a field as injectable with the enum member. The field
+//!     // type must be Injected<T>, where T is the type of the enum member's value.
+//!     #[inject(Model::Name)]
+//!     name: Injected<Rc<RefCell<String>>>,
+//!
+//!     #[inject(Model::Age)]
+//!     age: Injected<u32>
+//! }
+//!
+//! // This would represent a model
+//! let name = Rc::new(RefCell::new("Patje".to_string()));
+//! let age = 25;
+//!
+//! // This could be one of many views
+//! let mut injectee: Injectee = Default::default();
+//! injectee.inject(Model::Name(name));
+//! injectee.inject(Model::Age(age));
+//!
+//! // The injected fields can be accessed like normal references
+//! assert_eq!(&*injectee.name.borrow(), "Patje");
+//! assert_eq!(*injectee.age, 25);
+//! ```
+
+extern crate injectiny_proc_macro;
+
+use std::ops::{Deref, DerefMut};
+
+pub trait Injectable<T: Clone> {
+    fn inject(&mut self, value: T);
+}
+
+pub struct Injected<T> {
+    value: Option<T>
+}
+
+impl<T> Injected<T> {
+    pub fn from(value: T) -> Self {
+        Self {
+            value: Some(value)
+        }
+    }
+}
+
+impl<T> Default for Injected<T> {
+    fn default() -> Self {
+        Self {
+            value: None
+        }
+    }
+}
+
+impl<T> Deref for Injected<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.value.as_ref().unwrap()
+    }
+}
+
+impl<T> DerefMut for Injected<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.value.as_mut().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
---
+++ injectiny_proc_macro/src/lib.rs
@@ -0,0 +1,131 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use std::fmt::Debug;
+
+use quote::{quote, ToTokens};
+use syn;
+use syn::{Attribute, Data, DeriveInput, Field, parenthesized, parse_macro_input, Path};
+
+struct EnumMember
+{
+    path: Path
+}
+
+impl EnumMember
+{
+    fn has_enum_name(&self, path: &Path) -> bool
+    {
+        path.segments.iter().zip(self.path.segments.iter()).all(|(a, b)| {
+            a.ident == b.ident
+        })
+    }
+}
+
+impl Debug for EnumMember
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let segments = self.path.segments.iter().map(|segment| {
+            segment.ident.to_string()
+        }).collect::<Vec<_>>();
+        segments.join("::").fmt(f)
+    }
+}
+
+impl ToTokens for EnumMember
+{
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        self.path.to_tokens(tokens);
+    }
+}
+
+impl syn::parse::Parse for EnumMember
+{
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let content;
+        parenthesized!(content in input);
+        let path: Path = content.parse()?;
+        let segments: Vec<_> = path.segments.iter().map(|segment| {
+            segment.ident.to_string()
+        }).collect();
+
+        if segments.len() < 2 {
+            return Err(syn::Error::new_spanned(path, "Expected enum member to be of the form `Enum::Member`"));
+        }
+
+        Ok(Self { path })
+    }
+}
+
+fn get_inject_attrib_index(field: &Field) -> Option<usize>
+{
+    field.attrs.iter().position(|attr| {
+        if let Some(ident) = attr.path.get_ident() {
+            return ident == "inject";
+        }
+        else {
+            false
+        }
+    })
+}
+
+fn parse_injected_fields(ast: &mut DeriveInput) -> Vec<(&Field, Attribute)> {
+    let mut fields = vec![];
+
+    if let Data::Struct(data) = &mut ast.data {
+        for field in data.fields.iter_mut() {
+            if let Some(i) = get_inject_attrib_index(field) {
+                let attrib = field.attrs.remove(i);
+                fields.push((&*field, attrib));
+            }
+        }
+    }
+    else {
+        quote!(syn::Error::new_spanned(ast, "Injectable can only be applied to structs").to_compile_error().into());
+    }
+
+    fields
+}
+
+#[proc_macro_attribute]
+pub fn injectable(attr: TokenStream, input: TokenStream) -> TokenStream {
+    // TODO: Rewrite this to support #[inject(Enum::Field)] instead
+
+    let enum_val = parse_macro_input!(attr as Path);
+    let mut ast = parse_macro_input!(input as DeriveInput);
+    let fields: Vec<_> = parse_injected_fields(&mut ast);
+    let mut matches = core::default::Default::default();
+
+    for (field, attrib) in fields.into_iter() {
+        let field_name = field.ident.as_ref().unwrap();
+        // TODO: Find enum member that matches the field type
+
+        let tokens = attrib.tokens.into();
+        let member = parse_macro_input!(tokens as EnumMember);
+
+
+        if !member.has_enum_name(&enum_val) {
+            return quote!(syn::Error::new_spanned(ast, "All injected fields must be from the same enum").to_compile_error().into()).into();
+        }
+
+        matches = quote! {
+            #matches
+            #member(value) => self.#field_name = Injected::from(value),
+        };
+    }
+
+    let name = &ast.ident;
+    let quote = quote! {
+        #ast
+
+        impl ::injectiny::Injectable<#enum_val> for #name {
+            fn inject(&mut self, model: #enum_val) {
+                match model {
+                    #matches
+                    _ => {}
+                }
+            }
+        }
+    };
+    quote.into()
+}
